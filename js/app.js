1d36f20aa138c6e42cf1b0f842fe609b9241366a
juanCaro16 <carovargasjuandavid494@gmail.com>
2024-06-23 16:30:08 -0500

    dia del lector

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ const btnSort = document.getElementById('btnSort').addEventListener('click', sor
 const filterPages = document.getElementById('btn-pages').addEventListener('click', bookspages)
 const filterPrice =  document.getElementById('btn-prices').addEventListener('click', filterToPrices)
 const showExpensiveBooks = document.getElementById('btn-expensiveBooks').addEventListener('click', expensiveBooks)
-
+const ReadersDays =  document.getElementById('Readers-Day').addEventListener('click', ReadersDay)
 
 
 window.addEventListener('DOMContentLoaded', generateCard)
@@ -38,7 +38,7 @@ function makeCard(book) {
   titleBook.textContent = book.title
 
   let bookAuthor = document.createElement('h3')
-  bookAuthor.textContent = book.author
+  bookAuthor.textContent = `Autor: ${book.author}`
   bookAuthor.classList.add('authorBook')
 
   let bookDescription = document.createElement('p')
@@ -104,19 +104,23 @@ function filterToGender (event) {
     let titleBook = document.createElement('h2')
     titleBook.textContent = book.title
   
-    let author =  document.createElement('h3')
-    author.textContent = book.author
+    let bookAuthor =  document.createElement('h3')
+    bookAuthor.textContent = book.author
 
     let priceBook = document.createElement('h3')
     priceBook.textContent = `${book.price}COP`
   
     let btnBuy = document.createElement('button')
     btnBuy.textContent = 'Comprar'
+
+    btnBuy.addEventListener('click', function() {
+      window.location.href = 'login.html'; 
+    });
   
     bookPhoto.appendChild(imgBook)
     
     descriptionBook.appendChild(titleBook)
-    descriptionBook.appendChild(author)
+    descriptionBook.appendChild(bookAuthor)
     descriptionBook.appendChild(priceBook)
     descriptionBook.appendChild(btnBuy)
     
@@ -172,6 +176,74 @@ function expensiveBooks() {
   return books;
 }
 
+function ReadersDay() {
+    containerBooks.innerHTML = ''
+
+    books.forEach( books => {
+      makeCard(books)
+    })
+    
+    function makeCard(book) {
+      // Crear elementos de la card
+      let bookCard = document.createElement('div')
+      bookCard.classList.add('book')
+    
+      let bookPhoto = document.createElement('div')
+      bookPhoto.classList.add('book-photo')
+    
+      let imgBook = document.createElement('img')
+      imgBook.src = book.image
+      imgBook.alt = book.title
+    
+      let descriptionBook = document.createElement('div')
+      descriptionBook.classList.add('book-description')
+    
+      let titleBook = document.createElement('h2')
+      titleBook.textContent = book.title
+    
+      let bookAuthor = document.createElement('h3')
+      bookAuthor.textContent = `Autor: ${book.author}`
+      bookAuthor.classList.add('authorBook')
+    
+      let bookDescription = document.createElement('p')
+      bookDescription.textContent = book.description
+      bookDescription.classList.add('description')
+    
+      let pagesBook = document.createElement('h3')
+      pagesBook.textContent = `Paginas: ${book.pages}`
+
+      let normalPriceBook = document.createElement('h3')
+      normalPriceBook.textContent = `Precio normal: ${book.price}COP`
+      normalPriceBook.classList.add('normalPrice')
+    
+      let priceBook = document.createElement('h3')
+      priceBook.textContent = `Ahora: ${book.price * 0.90}COP`
+    
+      let btnBuy = document.createElement('button')
+      btnBuy.textContent = 'Comprar'
+    
+      btnBuy.addEventListener('click', function() {
+        window.location.href = 'login.html'; 
+      });
+    
+      bookPhoto.appendChild(imgBook)
+      
+      descriptionBook.appendChild(titleBook)
+      descriptionBook.appendChild(bookAuthor)
+      descriptionBook.appendChild(bookDescription)
+      descriptionBook.appendChild(pagesBook)
+      descriptionBook.appendChild(normalPriceBook)
+      descriptionBook.appendChild(priceBook)
+      descriptionBook.appendChild(btnBuy)
+      
+      bookCard.appendChild(bookPhoto)
+      bookCard.appendChild(descriptionBook)
+    
+      containerBooks.appendChild(bookCard)
+      return bookCard;
+    }
+}
+
 function filterToPrices() {
   containerBooks.innerHTML = ''
 
@@ -199,19 +271,23 @@ function filterToPrices() {
     let titleBook = document.createElement('h2')
     titleBook.textContent = book.title
   
-    let author =  document.createElement('h3')
-    author.textContent = book.author
+    let bookAuthor =  document.createElement('h3')
+    bookAuthor.textContent = `Autor: ${book.author}`
+    
 
     let priceBook = document.createElement('h3')
     priceBook.textContent = `${book.price}COP`
   
     let btnBuy = document.createElement('button')
     btnBuy.textContent = 'Comprar'
+    btnBuy.addEventListener('click', function() {
+      window.location.href = 'login.html'; 
+    });
   
     bookPhoto.appendChild(imgBook)
     
     descriptionBook.appendChild(titleBook)
-    descriptionBook.appendChild(author)
+    descriptionBook.appendChild(bookAuthor)
     descriptionBook.appendChild(priceBook)
     descriptionBook.appendChild(btnBuy)
     
@@ -252,19 +328,24 @@ function bookspages() {
     let titleBook = document.createElement('h2')
     titleBook.textContent = book.title
   
-    let author =  document.createElement('h3')
-    author.textContent = book.author
+    let bookAuthor =  document.createElement('h3')
+    bookAuthor.textContent = `Autor: ${book.author}`
 
     let priceBook = document.createElement('h3')
     priceBook.textContent = `${book.price}COP`
   
     let btnBuy = document.createElement('button')
     btnBuy.textContent = 'Comprar'
+    btnBuy.addEventListener('click', function() {
+      window.location.href = 'login.html'; 
+    });
   
+
+
     bookPhoto.appendChild(imgBook)
     
     descriptionBook.appendChild(titleBook)
-    descriptionBook.appendChild(author)
+    descriptionBook.appendChild(bookAuthor)
     descriptionBook.appendChild(priceBook)
     descriptionBook.appendChild(btnBuy)
     
